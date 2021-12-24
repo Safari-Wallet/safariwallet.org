3cd9be529dcb219965dff47e1c7b1d710b963857
natclark <39421782+natclark@users.noreply.github.com>
2021-12-24 01:20:11 -0600

    redirect community bit.ly link to discord invite

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,7 +29,7 @@
                 </span>
             </p>
             <div class="px-8 sm:items-center sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
-                <a class="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" href="https://bit.ly/safari-wallet-community" rel="external noopener" target="_blank">
+                <a class="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" href="https://bit.ly/3H8wf9k" rel="external noopener" target="_blank">
                     Join Community
                 </a>
                 <a class="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 border-gray-400 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 focus:bg-gray-900 hover:text-white focus:text-white hover:border-gray-900 focus:border-gray-900" href="/forks/" sveltekit:prefetch>
